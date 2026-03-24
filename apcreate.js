1783088b52fd37a6b07c66e73d50261dbf199fb2
DanteM-max <DanteMatanza1301@gmail.com>
2026-03-24 21:05:55 +0000

    Got some html

---
+++ apcreate.js
@@ -0,0 +1,7 @@
+let userInputParent = document.getElementById("array-maker");
+let nArea = document.getElementById("item-count");
+function makeBoxes(n) {
+    for (let i = 0; i < n; i++) {
+        let box = document.createElement()
+    }
+}

ee19b0c4b4fa22731ff096666786ac5d32757531
DanteM-max <DanteMatanza1301@gmail.com>
2026-03-25 21:07:15 +0000

    Making koolaid

--- apcreate.js
+++ apcreate.js
@@ -1,7 +1,41 @@
 let userInputParent = document.getElementById("array-maker");
 let nArea = document.getElementById("item-count");
-function makeBoxes(n) {
+function makeBoxes() {
+    let n = parseInt(document.getElementById('item-count').value);
     for (let i = 0; i < n; i++) {
-        let box = document.createElement()
+        let box = document.createElement("input");
+        box.type = "number";
+        box.classList.add("arrayNumber");
+        box.placeholder = "a value for the array";
+        userInputParent.appendChild(box);
+    }
+    let apCreateTaskFinalizerButton = document.createElement("button");
+    userInputParent.appendChild(apCreateTaskFinalizerButton);
+    apCreateTaskFinalizerButton.innerText = "Organize";
+    apCreateTaskFinalizerButton.addEventListener("click", null);
+}
+
+function collectArray() {
+    let arrayToReturn = [];
+    for (let i = 0; i < document.getElementsByClassName("arrayNumber").length; i++) {
+        arrayToReturn.push(parseInt(document.querySelectorAll(".arrayNumber")[i].value));
+    }
+
+    return(arrayToReturn);
+}
+
+function sortArray(arr) {
+    let sortedArray = [];
+    console.log(sortedArray);
+    let lengthOfArr = arr.length;
+    console.log(lengthOfArr);
+    let minimum = arr[0];
+    for (let i = 0; i < lengthOfArr; i++) {
+        for (let jet = 0; jet < arr.length; jet++) {
+            if (arr[jet] < minimum) {
+                minimum = arr[jet];
+            }
+        sortedArray.push(arr.pop(indexOf(minimum)));
+        }
     }
 }
